75594717e3fc63d43fba175a9b4d46bb8ebfefb0
Dominik Liehr <dominik_liehr@me.com>
2023-07-14 13:09:21 +0200

    - initial commit

---
+++ src/main/java/de/liehrit/legocollectorapi/LegocollectorapiApplication.java
@@ -0,0 +1,13 @@
+package de.liehrit.legocollectorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegocollectorapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegocollectorapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/liehrit/legocollectorapi/LegocollectorapiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.liehrit.legocollectorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegocollectorapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
